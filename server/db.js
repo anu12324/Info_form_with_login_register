003996f416a716c9ee4f68e5db753216f4827ff8
anu12324 <anuragg2053@gmail.com>
2025-07-24 22:26:06 +0530

    a

--- server/db.js
+++ server/db.js
@@ -1,30 +1,30 @@
-// const { Pool } = require('pg');
-// require('dotenv').config();
-
-// const db = new Pool({
-//     user: process.env.DB_USER,
-//     host: process.env.DB_HOST,
-//     database: process.env.DB_NAME,
-//     password: process.env.DB_PASSWORD,
-//     port: process.env.DB_PORT,
-//     ssl: {
-//         rejectUnauthorized: false,
-//     }
-// });
-
-// db.connect();
-
-// module.exports = db;
-
-// require('dotenv').config();
-require('dotenv').config({ override: true });   
 const { Pool } = require('pg');
+require('dotenv').config();
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+const db = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
     ssl: {
-        rejectUnauthorized: false,  // Required for Render-hosted DBs
-    },
+        rejectUnauthorized: false,
+    }
 });
 
-module.exports = pool;
+db.connect();
+
+module.exports = db;
+
+// require('dotenv').config();
+// require('dotenv').config({ override: true });   
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false,  // Required for Render-hosted DBs
+//     },
+// });
+
+// module.exports = pool;
